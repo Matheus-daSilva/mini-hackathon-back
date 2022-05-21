17b6ac7059d5b8e2e5cb575b448e03203edb389e
João Sarmento <joaovicentesarm@gmail.com>
2022-05-20 22:26:37 -0300

    schema question ok

--- src/middlewares/questionMiddleware.js
+++ src/middlewares/questionMiddleware.js
@@ -3,12 +3,14 @@ import Joi from 'joi';
 const schema =  Joi.object({
     name: Joi.string().min(1).required(),
     question: Joi.string().min(1).required(),
-    subject: Joi.string().valid(...['Português', 'Matemática', 'História', 'Geografia', 'Ciências da natureza', 'Ciências sociais']).required(),
+    subject: Joi.string().valid('Português', 'Matemática', 'História', 'Geografia', 'Ciências da natureza', 'Ciências sociais').required()
 })
 
 export const postQuestionMiddleware = (req, res, next) => {
-    if(!schema.validate(req.body))
-        return res.sendStatus(422)
+    const {error} = schema.validate(req.body);
+    if(error) return res.sendStatus(422);
+
+    return res.sendStatus(200);
 
     next()
 }
